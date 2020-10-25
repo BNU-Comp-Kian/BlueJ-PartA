148d89a47df575ee6ee1cd8dda38480230518c79
kianroz122 <50154724+kianroz122@users.noreply.github.com>
2020-10-25 15:01:03 +0000

    Create Course.java

---
+++ app03-studentGrades/Course.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Course here.
+ *
+ * @author (Kian Rozblat)
+ * @version (24/10/2020)
+ */
+public class Course
+{
+    // instance variables - replace the example below with your own
+    private static final int F = 0-39;
+    private static final int D = 40-49;
+    private static final int C = 50-59;
+    private static final int B = 60-69;
+    private static final int A = 70-100;
+    private String course ;
+    /**
+     * Constructor for objects of class Course
+     */
+    public Course()
+    {
+        // initialise instance variables
+        this.course=course;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void no()
+    {
+        // put your code here
+        
+    }
+}
