47bd54ae66a6aa147cf2644fc1ae7896d0fe5f05
kianroz122 <50154724+kianroz122@users.noreply.github.com>
2020-10-25 15:01:09 +0000

    Create Module.java

---
+++ app03-studentGrades/Module.java
@@ -0,0 +1,39 @@
+import java.util.*;
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+    private String module1;
+    private String module2;
+    private String module3;
+    private String module4;
+    private int marks;
+    /**
+     * Constructor for objects of class Module
+     */
+    public void modules()
+    {
+        // setting all modules
+        this.module1 = module1;
+        this.module2 = module2;
+        this.module3 = module3;
+        this.module4 = module4;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void marks(int y)
+    {
+        // put your code here
+        marks = 0;
+    }
+}
